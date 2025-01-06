331e8c9e155466918ee908a649eca3f67fb16f6d
Driss Chraibi <Chraibidriss001@gmail.com>
2025-01-07 00:57:15 +0100

    New

--- project_config.py
+++ project_config.py
@@ -10,12 +10,6 @@ _ = load_dotenv(find_dotenv())
 dotenv_path = find_dotenv()
 
 OPENAI_API_KEY = os.environ['OPENAI_API_KEY']  # Change to your space name
-CONFLUENCE_API_KEY = os.environ['CONFLUENCE_PRIVATE_API_KEY']
-# https://support.atlassian.com/atlassian-account/docs/manage-api-tokens-for-your-atlassian-account/
-CONFLUENCE_SPACE_KEY = os.environ['CONFLUENCE_SPACE_KEY']
-# Hint: space_key and page_id can both be found in the URL of a page in Confluence
-# https://yoursite.atlassian.com/wiki/spaces/<space_key>/pages/<page_id>
-CONFLUENCE_USERNAME = os.environ['EMAIL_ADRESS']
 PATH_NAME_SPLITTER = './splitted_docs.jsonl'
 PERSIST_DIRECTORY = './db/chroma/'
 EVALUATION_DATASET = '../data/evaluation_dataset.tsv'
